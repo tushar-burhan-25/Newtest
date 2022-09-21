e52ae31d96379a807a51dcde867385f7506bc09a
tushar-burhan-amla <94620980+tushar-burhan-amla@users.noreply.github.com>
2022-09-21 14:16:44 +0530

    “comment”

---
+++ core/BulkyBook/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="The Display Order is 1 to 100 only.")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
